a89f191d16957617f05e2f2a627a82044506cdea
Daniel Fernández <defp_99@hotmail.com>
2023-07-11 14:13:57 -0600

    Test1

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="public/img/1200px-Tec-Tuxtla_Logo.svg.png" alt="">
+</template>
---
+++ resources/js/Pages/Views/academicos/Index.Detecciones.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import {Head} from "@inertiajs/vue3";
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <Head title="Detecciones"/>
+        <span>
+            Aqui sera la tabla e interfaz
+        </span>
+        <div class="container mx-auto">
+
+        </div>
+
+    </AuthenticatedLayout>
+</template>
